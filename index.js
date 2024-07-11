08f4990895adce9f13f9f6a9c5fb8bc0e9ad989d
sumitrawat30aug@gmail.com <33266129+SUMIT30AUGUST@users.noreply.github.com>
2024-07-11 08:14:35 +0530

    socket io error is present

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path  = require('path');
+const path = require('path');
 const app = express();
 const http = require('http');
 const scoketio = require('socket.io')
@@ -8,10 +8,15 @@ const server = http.createServer(app);
 const io = scoketio(server);
 
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, 'public')))
+// app.set(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on("connection", (socket) => {
+    console.log("connected")
+})
 
 app.get("/", (req, res) => {
-    res.send("hello");
+    res.render("index");
 })
 
 app.listen(5000, () => console.log("server running at 5000 port"))
